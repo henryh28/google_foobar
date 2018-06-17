b8036440b8c72014a8bec00650efae44e90af0e0
henryh28 <h_hsu817@yahoo.com>
2018-06-17 07:46:22 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Google Foo.bar challenges
+
+Has to be done in Python 2.7.6 instead of Python 3.x
+
+|             *Filename*             |                *Description*                                            |
+|------------------------------------|-------------------------------------------------------------------------|
+| i_love_lance_janice.py             | Function to decrypt a string's lowercase letters into its inverse       |
+
+
+## Author
+Henry Hsu
---
+++ i_love_lance_janice.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+from __future__ import print_function
+
+def answer(s):
+    key_list = [chr(i) for i in range(ord('a'), ord('z') + 1)]
+    value_list = [chr(j) for j in range(ord('z'), ord('a') - 1, -1)]
+    my_dict = dict(list(zip(key_list, value_list)))
+
+    decrypt = []
+
+    for character in s:
+        decrypt.append(my_dict[character] if character in my_dict else character)
+    return ''.join(decrypt)
+
+
+test_1 = "wrw blf hvv ozhg mrtsg'h vkrhlwv?"
+# (string1) "did you see last night's episode?"
+test_2 = "Yvzs! I xzm'g yvorvev Lzmxv olhg srh qly zg gsv xlolmb!!"
+# (string2) "Yeah! I can't believe Lance lost his job at the colony!!"
+
+print(answer(test_1))
+print(answer(test_2))
